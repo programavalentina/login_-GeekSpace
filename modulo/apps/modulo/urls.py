106789c6937904c59fb5d88d6f7bf51d1fa83585
hamiltongustavo <44583639+hamiltongustavo@users.noreply.github.com>
2018-10-30 22:02:16 -0600

    intento4

---
+++ modulo/apps/modulo/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.contrib.auth.views import logout_then_login
+from django.contrib.auth.decorators import login_required
+from django.contrib import admin
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    ]

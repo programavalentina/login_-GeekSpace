430d39c1fb3bac78ef61fa4ceba8de678b71c707
Edilson Jimenez <edilsonjimenezg@gmail.com>
2018-11-12 07:10:41 -0600

    Users CRUD
    Crate, Update, Delete

--- modulo/apps/users/urls.py
+++ modulo/apps/users/urls.py
@@ -11,8 +11,14 @@ urlpatterns = [
     path('register', views.RegistroUsuario.as_view(), name='register'),
     path('profile', login_required(views.profile, 'modulo:index', '/users/login') , name='profile'),
     path('logout', LogoutView.as_view(template_name='modulo/index.html'), name='logout'),
-    path('courses', views.courses, name='courses'),
-    path('create-course', views.CreateCourse.as_view(), name='create-course'),
-    path('update-course/<int:pk>/', views.UpdateCourse.as_view(), name='update-course'),
-    path('delete-course/<int:pk>/', views.DeleteCourse.as_view(), name='delete-course'),
+    path('courses',  login_required(views.courses, 'modulo:index', '/users/login') , name='courses'),
+    path('create-course', login_required(views.CreateCourse.as_view(), 'modulo:index', '/users/login') , name ='create-course'),
+    path('update-course/<int:pk>/',login_required(views.UpdateCourse.as_view(), 'modulo:index', '/users/login')  , name='update-course'),
+    path('delete-course/<int:pk>/', login_required(views.DeleteCourse.as_view(), 'modulo:index', '/users/login') , name='delete-course'),
+    path('users', login_required(views.users,'modulo:index', '/users/login'),  name='users'),
+    path('create-user', login_required(views.CreateUser.as_view(), 'modulo:index', '/users/login') , name='create-course'),
+    path('update-user/<int:pk>/', login_required(views.UpdateUser.as_view(), 'modulo:index', '/users/login') , name='update-course'),
+    path('delete-user/<int:pk>/', login_required(views.DeleteUser.as_view(), 'modulo:index', '/users/login') , name='delete-course'),
     ]
+
+
